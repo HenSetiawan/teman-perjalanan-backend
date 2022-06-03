41452e73f34a9b0d7e043a245cfc184f43c0b62b
hensetiawan <hendystyawan74@gmail.com>
2022-06-03 08:56:17 +0700

    add get detail destinations

--- src/controllers/destination-controller.js
+++ src/controllers/destination-controller.js
@@ -1,6 +1,4 @@
 const supabaseService = require('../supabase/supabase-service');
-const fs = require('fs');
-const path = require('path');
 
 exports.getAllDestinations = async (req, res) => {
   try {
@@ -16,7 +14,10 @@ exports.addNewDestination = async (req, res) => {};
 exports.deleteDestination = async (req, res) => {
   const idDestination = req.params.id;
   try {
-    const result = await supabaseService.deleteDataById('wisata', idDestination);
+    const result = await supabaseService.deleteDataById(
+      'wisata',
+      idDestination
+    );
     if (result.data.length < 1) {
       res.status(404).json({ message: 'error data not found', result });
     } else {
@@ -24,5 +25,22 @@ exports.deleteDestination = async (req, res) => {
     }
   } catch (error) {
     res.json({ message: 'error', error });
+  }
+};
+
+exports.getDetailDestination = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const destination = await supabaseService.getSpecificData(
+      'wisata',
+      'id',
+      id
+    );
+    return res.status(200).json({
+      message: 'success',
+      destination: { destination },
+    });
+  } catch (error) {
+    res.status(400).json({ message: 'error', error });
   }
 };
