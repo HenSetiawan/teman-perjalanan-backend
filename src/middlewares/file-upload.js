0e96a679642d427a8cdbf8bf94a9e563101d6f19
hensetiawan <hendystyawan74@gmail.com>
2022-06-06 17:53:45 +0700

    add image filter validation

--- src/middlewares/file-upload.js
+++ src/middlewares/file-upload.js
@@ -13,6 +13,19 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const filter = (req, file, cb) => {
+  if (
+    file.mimetype == 'image/png' ||
+    file.mimetype == 'image/jpg' ||
+    file.mimetype == 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+    return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: filter });
 
 module.exports = upload;
