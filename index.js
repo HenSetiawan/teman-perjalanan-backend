404b9716007d293cf18654015d4cf1718e710a65
hensetiawan <hendystyawan74@gmail.com>
2022-06-09 13:51:36 +0700

    add port on env

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { route } = require('./src/routers/route');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', route);
 
-app.listen(3000, () => console.log(`server running on port 3000`));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
